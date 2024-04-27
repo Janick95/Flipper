f8c4ac610adb7e897bb6fbb2f01c452822bf7cbb
Norox95 <janick.metzger@stud.hshl.de>
2024-04-27 17:20:08 +0200

    Running

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ pygame.init()
 
 
 print ("Blub")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
