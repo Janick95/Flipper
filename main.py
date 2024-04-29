db59bb2fe29676873fc517b00646b291fe727f68
Norox95 <janick.metzger@stud.hshl.de>
2024-04-29 20:22:46 +0200

    Ball gezeichnet

--- main.py
+++ main.py
@@ -7,13 +7,28 @@ print ("Blub")
 
 window_width = 800
 window_height = 600
+
+circlecolor = (200, 0, 0)
+circleX = 100
+circleY = 100
+radius = 10
+
+
 window = pygame.display.set_mode((window_width, window_height))
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        window.fill((25,25,25))
+        
+        pygame.draw.circle(window, circlecolor,(circleX, circleY),radius)
+
+        pygame.display.update()
+
 
 pygame.quit()
 
