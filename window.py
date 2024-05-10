d33f9ba657e48927ca7d26b37f9dc14c8fac4a76
Norox95 <janick.metzger@stud.hshl.de>
2024-05-10 16:28:48 +0200

    window_comments

--- window.py
+++ window.py
@@ -1,16 +1,16 @@
 import pygame
 
 class Window:
-    WINDOWWIDTH = 800 #
-    WINDOWHEIGHT = 1000
-    CAPTION = "2D Flipper Automat"
+    WINDOWWIDTH = 800 #WINDOWWIDTH (int): The width of the window
+    WINDOWHEIGHT = 1000 #WINDOWHEIGHT (int): The height of the window
+    CAPTION = "2D Flipper Automat" #CAPTION (str): The caption of the window
 
 
     
-    
+    #__init__(self): Initializes the window
     def __init__(self):
-        self.window = pygame.display.set_mode((self.WINDOWWIDTH, self.WINDOWHEIGHT))
-        pygame.display.set_caption(self.CAPTION)
+        self.window = pygame.display.set_mode((self.WINDOWWIDTH, self.WINDOWHEIGHT)) #create the window
+        pygame.display.set_caption(self.CAPTION) #set the caption of the window
 
    # Calculate the window resolution into real units
    # def realUnits(realValue):
